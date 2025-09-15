44593c4a4478c803391165c6cdce744025f6efa6
mili323 <nandana.subhash@edu.rtu.lv>
2025-09-15 08:28:17 +0000

    Adjust formatting for hello world

--- src/hello.c
+++ src/hello.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-  // This is my first C program  in github
+  // this is my first code
   printf("Hello, My name is nandu!\n");
   printf("You passed %d argument(s).\n", argc - 1);
   for (int i = 1; i < argc; ++i) {
